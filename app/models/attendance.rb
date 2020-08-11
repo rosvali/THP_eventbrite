9791342052ad50169f61961323162a314b72babe
rosvali <rosval6578@gmail.com>
2020-08-11 19:31:34 +0200

    lol

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,7 +1,7 @@
 class Attendance < ApplicationRecord
     belongs_to :user
     belongs_to :event
-   after_create :participant_send
+   # after_create :participant_send
 
      def participant_send
          AttendanceMailer.welcome_participant(self).deliver_now
