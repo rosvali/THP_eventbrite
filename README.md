8251258ad725fec2fab56cd0c51ceb0003ffec8f
rosvali <rosval6578@gmail.com>
2020-08-11 19:26:12 +0200

    lol

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## Projet de The Hacking Project
 Ce projet consiste à recréer eventbrite en plus simplifié
+Accéder à l'appli : https://rocky-plains-57010.herokuapp.com
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
     has_many :attendances
     has_many :events, through: :attendances
 
-    after_create :welcome_send
+    # after_create :welcome_send
 
     def welcome_send
       UserMailer.welcome_email(self).deliver_now
